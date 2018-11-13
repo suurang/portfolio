39033207ff5c91845cfd791b0d2503c1840160c1
sujin <kimsujin0425@naver.com>
2018-11-13 21:47:39 +0900

    'susu'

--- js/timer.js
+++ js/timer.js
@@ -1,4 +1,14 @@
+/*
 setTimeout(function () {
 	var loading = document.querySelector('.loading-box');
 	loading.remove();
 }, 3000);
+*/
+
+
+function soso () {
+	var loading = document.querySelector('.loading-box');
+	loading.remove();
+}
+
+setTimeout(soso, 3000);
